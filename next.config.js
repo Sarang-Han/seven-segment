fbda8f2f63bd1fb935721bb6112a3fb40c796056
Sarang <hangpfm0518@ewhain.net>
2025-04-15 20:57:05 +0900

    수정

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // Static HTML 내보내기 활성화
-  images: {
-    unoptimized: true, // 이미지 최적화 비활성화
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/seven-segment' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/seven-segment/' : '',
 }
 
 module.exports = nextConfig
